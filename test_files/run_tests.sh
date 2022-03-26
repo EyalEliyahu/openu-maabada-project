5936781bc3902b9c4515ec029bbdf5f715a04d51
eyaliyahu <eyal@jit.io>
2022-03-26 15:38:25 +0300

    fix first pas

--- test_files/run_tests.sh
+++ test_files/run_tests.sh
@@ -7,15 +7,12 @@ tests_folder=$PWD
 cd ..
 make
 cd $tests_folder
-
-for as_file in *.as
-do
-  file_prefix=as_file
+  as_file="valid_input.as"
   file_prefix=${as_file/.as/}
   echo "Running assembler on $as_file:"
   printf "\n"
   echo "-------------------------- Assembler Output: --------------------------"
-  ../assembler $file_prefix
+  ../assembler $file_prefix > "$as_file.result"
   echo "-------------------------- End Of Assembler Output --------------------------"
   printf "\n"
   echo "Loking for diff in $as_file:"
@@ -25,4 +22,31 @@ do
       diff "$file_prefix.$i" "$file_prefix.$prefix_of_extension.$i"
     done
       printf "\n\n"
-done
+
+
+# declare -a file_extensions=("ob" "ext" "ent")
+# prefix_of_extension="expected"
+
+# tests_folder=$PWD
+# cd ..
+# make
+# cd $tests_folder
+
+# for as_file in *.as
+# do
+#   file_prefix=as_file
+#   file_prefix=${as_file/.as/}
+#   echo "Running assembler on $as_file:"
+#   printf "\n"
+#   echo "-------------------------- Assembler Output: --------------------------"
+#   ../assembler $file_prefix
+#   echo "-------------------------- End Of Assembler Output --------------------------"
+#   printf "\n"
+#   echo "Loking for diff in $as_file:"
+#     for i in "${file_extensions[@]}"
+#     do
+#       echo "  Loking for diff in $file_prefix.$i:"
+#       diff "$file_prefix.$i" "$file_prefix.$prefix_of_extension.$i"
+#     done
+#       printf "\n\n"
+# done
